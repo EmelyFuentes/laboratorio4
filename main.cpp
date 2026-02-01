72400235b94dc97d6f0a96ed36d21e80e1dadc9d
Rodrix-ra34 <00167625@uca.edu.com>
2026-01-31 20:19:51 -0600

    add:se agrego la funcion para buscar por medio del id

--- main.cpp
+++ main.cpp
@@ -3,62 +3,78 @@
 
 using namespace std;
 
-struct Nodo {
+struct Nodo
+{
     int id;
     string nombre;
     float peso;
-    Nodo* sig;
-    Nodo* ant;
+    Nodo *sig;
+    Nodo *ant;
 };
 
 // Prototipos
-bool existeId(Nodo* head, int id);
-void insertarInicio(Nodo*& head, Nodo*& tail, int id, string nombre, float peso);
-void insertarFinal(Nodo*& head, Nodo*& tail, int id, string nombre, float peso);
-void mostrarAdelante(Nodo* head);
-void mostrarAtras(Nodo* tail);
+bool existeId(Nodo *head, int id);
+void insertarInicio(Nodo *&head, Nodo *&tail, int id, string nombre, float peso);
+void insertarFinal(Nodo *&head, Nodo *&tail, int id, string nombre, float peso);
+void mostrarAdelante(Nodo *head);
+void mostrarAtras(Nodo *tail);
+Nodo *buscarPorId(Nodo *head, int id);
 
 // ---------------- FUNCIONES ----------------
 
-bool existeId(Nodo* head, int id) {
-    Nodo* aux = head;
-    while (aux != NULL) {
-        if (aux->id == id) return true;
+bool existeId(Nodo *head, int id)
+{
+    Nodo *aux = head;
+    while (aux != NULL)
+    {
+        if (aux->id == id)
+            return true;
         aux = aux->sig;
     }
     return false;
 }
 
-void insertarInicio(Nodo*& head, Nodo*& tail, int id, string nombre, float peso) {
-    Nodo* nuevo = new Nodo{id, nombre, peso, head, NULL};
+void insertarInicio(Nodo *&head, Nodo *&tail, int id, string nombre, float peso)
+{
+    Nodo *nuevo = new Nodo{id, nombre, peso, head, NULL};
 
-    if (head == NULL) {
+    if (head == NULL)
+    {
         head = tail = nuevo;
-    } else {
+    }
+    else
+    {
         head->ant = nuevo;
         head = nuevo;
     }
 }
 
-void insertarFinal(Nodo*& head, Nodo*& tail, int id, string nombre, float peso) {
-    Nodo* nuevo = new Nodo{id, nombre, peso, NULL, tail};
+void insertarFinal(Nodo *&head, Nodo *&tail, int id, string nombre, float peso)
+{
+    Nodo *nuevo = new Nodo{id, nombre, peso, NULL, tail};
 
-    if (tail == NULL) {
+    if (tail == NULL)
+    {
         head = tail = nuevo;
-    } else {
+    }
+    else
+    {
         tail->sig = nuevo;
         tail = nuevo;
     }
 }
 
-void mostrarAdelante(Nodo* head) {
-    if (head == NULL) {
+void mostrarAdelante(Nodo *head)
+{
+    if (head == NULL)
+    {
         cout << "Lista vacia.\n";
         return;
     }
 
-    Nodo* aux = head;
-    while (aux != NULL) {
+    Nodo *aux = head;
+    while (aux != NULL)
+    {
         cout << "[" << aux->id << "] "
              << aux->nombre << " - "
              << aux->peso << "kg\n";
@@ -66,17 +82,32 @@ void mostrarAdelante(Nodo* head) {
     }
 }
 
-void mostrarAtras(Nodo* tail) {
-    if (tail == NULL) {
+void mostrarAtras(Nodo *tail)
+{
+    if (tail == NULL)
+    {
         cout << "Lista vacia.\n";
         return;
     }
 
-    Nodo* aux = tail;
-    while (aux != NULL) {
+    Nodo *aux = tail;
+    while (aux != NULL)
+    {
         cout << "[" << aux->id << "] "
              << aux->nombre << " - "
              << aux->peso << "kg\n";
         aux = aux->ant;
     }
+}
+
+Nodo *buscarPorId(Nodo *head, int id)
+{
+    Nodo *aux = head;
+    while (aux != NULL)
+    {
+        if (aux->id == id)
+            return aux;
+        aux = aux->sig;
+    }
+    return NULL;
 }
